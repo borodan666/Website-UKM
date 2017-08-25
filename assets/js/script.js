d393c4bbe3c9d691f28c7014247468b1f49c2780
Unknown <dianpratama2@gmail.com>
2017-08-25 15:00:03 +0800

    MANTAP JIWA

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 59)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 300
+        }
+    });
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+    sr.reveal('.sr-image', {
+        duration: 600,
+        scale: 0.7,
+        distance: '0px'
+    }, 200);
+
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+    
+    $(document).scroll(function(){
+        if($(this).scrollTop() > 300){
+            $('.backTop').fadeIn();
+        }else{
+            $('.backTop').fadeOut();
+        }
+        
+    });
+    
+    // var rellax = new Rellax('.rellax');
+    
+    // $(window).scroll(function(){
+    //    var scroll = $(this).scrollTop();
+        
+    //     if(scroll > $('.section-heading').offset().top - 200 ){
+    //         $('.section-heading').addClass('muncul');
+    //     }
+    // });
+
+
+})(jQuery); // End of use strict
